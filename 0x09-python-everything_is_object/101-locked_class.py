4a9d79f092ccf9a5e8948c971396f8769d807d23
“Stephen <smartstevek@gmail.com>
2023-01-05 14:39:09 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-LockedClass = __import__('101-locked_class').LockedClass
+"""
+This is a module that containts a clas that avoids
+dynmaically created attributes
+"""
 
-lc = LockedClass()
-lc.first_name = "John"
-try:
-    lc.last_name = "Snow"
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+        pass
